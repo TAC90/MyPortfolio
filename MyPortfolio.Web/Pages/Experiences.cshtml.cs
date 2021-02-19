4d0a2a1e44d1bd2c9015d82f18129a8005ee4e50
TAC90 <48682971+TAC90@users.noreply.github.com>
2021-02-19 15:18:00 +0100

    PostEdit and Experiences working

--- MyPortfolio.Web/Pages/Experiences.cshtml.cs
+++ MyPortfolio.Web/Pages/Experiences.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using MyPortfolio.Data;
 using MyPortfolio.Data.Entities;
 
@@ -22,12 +23,14 @@ namespace MyPortfolio.Web.Pages
 
         public void OnGet()
         {
+            int languageId = 1; //TODO: Culture/language selection implementation
             //Get (numbered) list of blog posts with titles.
-            //Create a viewmodel with required model data? Is this my viewmodel?
-            using (_context)
-            {
-                Posts = _context.Posts.Where(p => p.CategoryId == 1).ToList();
-            }
+            Posts = _context.Posts
+                .Where(p => p.CategoryId == 1)
+                .Include(p => p.Content)
+                .Where(p => p.Content.Count != 0 && p.Public == true)
+                .Where(p => p.Content.Any(c => c.LanguageId == languageId))
+                .ToList();
         }
     }
 }
--- MyPortfolio.Web/Pages/PostEdit.cshtml.cs
+++ MyPortfolio.Web/Pages/PostEdit.cshtml.cs
@@ -17,6 +17,7 @@ namespace MyPortfolio.Web.Pages
         [BindProperty]
         public Post Post { get; set; }
         public List<SelectListItem> Categories { get; set; }
+        //[BindProperty]
         public List<Language> Languages { get; set; }
         public PostEditModel(PortfolioContext context)
         {
@@ -30,14 +31,8 @@ namespace MyPortfolio.Web.Pages
             //    //What to do when none found?
             //    //Do I need `using` during these database inquiries and otherwise, or does DI handle this automatically?
             //}
-            Categories = _context.Categories.Select(c =>
-            new SelectListItem
-            {
-                Value = c.Id.ToString(),
-                Text = c.Name
-            }).ToList();
-            Languages = _context.Languages.ToList();
-            Post = _context.Posts.Where(p => p.Id == id).Include(p => p.Content).FirstOrDefault();
+            PopulateOptions();
+            Post = _context.Posts.Where(p => p.Id == id).Include(p => p.Content).ThenInclude(c => c.Language).FirstOrDefault();
             var Contents = Post.Content ?? new List<Content>();
             //Check if content list has all languages
             if (Contents.Count != Languages.Count)
@@ -51,21 +46,40 @@ namespace MyPortfolio.Web.Pages
                         //Add new content object to list if none found
                         Contents.Add(new Content
                         {
+                            Language = Languages[i],
                             LanguageId = Languages[i].Id,
                             PostId = Post.Id
                         });
                     }
                 }
+                //_context.Update(Post);
+                //_context.SaveChanges();
             }
+            Post.Content = Contents;
             Console.WriteLine(string.Empty);
         }
+
+        private void PopulateOptions()
+        {
+            Categories = _context.Categories.Select(c =>
+            new SelectListItem
+            {
+                Value = c.Id.ToString(),
+                Text = c.Name
+            }).ToList();
+            Languages = _context.Languages.ToList();
+        }
+
         public void OnPost()
         {
+            Post.Modified = DateTime.Now;
+            //TODO: Check if content text was modified, update if so. Use StateChanged event?
             _context.Update(Post);
             //Update both changes individually, or will updating post also add the content?
             //_context.Update(Contents);
-            //_context.SaveChanges();
+            _context.SaveChanges();
             Console.WriteLine(string.Empty);
+            PopulateOptions();
         }
     }
 }
